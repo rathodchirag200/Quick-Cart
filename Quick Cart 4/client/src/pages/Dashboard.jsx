c157400a703d570e525cd4ab68bbf75842d1cdc2
Rathod chirag <cjrathod12345@gmail.com>
2025-12-30 22:58:46 +0530

    Add full ecommerce project

--- Quick Cart 4/client/src/pages/Dashboard.jsx
+++ Quick Cart 4/client/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Hero } from "../Componenets/Hero";
-import { Products } from "../Componenets/products";
+import { Products } from "../Componenets/Products";
 import { Bestseller } from "../Componenets/Bestseller";
 import { Subscribe } from "../Componenets/subscribe";
 import { Popular } from "../Componenets/popular";
